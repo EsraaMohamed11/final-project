8ba993fb8f64d11f791ac89776ce4c159cf48f19
EsraaMohamed11 <esraamohamedsh555@gmail.com>
2024-10-04 21:19:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// start BEDS products
+let bedsProducts=[
+    {
+        id: 1,
+        name: "Delia King Size Bed, Marine Velvet",
+        price: 399.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/26.jpg",
+        category: "bed",
+    },
+    {
+        id: 2,
+        name: "Essentials Kano Platform Double Bed",
+        price: 449.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/23.jpg",
+        category: "bed",
+    },
+    {
+        id: 3,
+        name: "Hyron Guest Bed with 2 Mattresses",
+        price: 339.00,  //after discount //before discount 449
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/25-600x450.jpg",
+        category: "bed",
+    },
+    {
+        id: 4,
+        name: "Kavaro 3000 Pocket Natural Kingsize",
+        price: 489.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/22.jpg://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/25-600x450.jpg",
+        category: "bed",
+    },
+    {
+        id: 5,
+        name: "Roscoe King Size Bed, Aegean Blue",
+        price: 429.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/24.jpg",
+        category: "bed",
+    },
+]
+// end BEDS products
