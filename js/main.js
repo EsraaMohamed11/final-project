a59e8671fefa6671eb2a046043cb228adc48772c
EsraaMohamed11 <esraamohamedsh555@gmail.com>
2024-10-06 00:48:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // start BEDS products
 let bedsProducts=[
     {
-        id: 1,
+        id:5001,
         name: "Delia King Size Bed, Marine Velvet",
         price: 399.00,
         description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
@@ -9,7 +9,7 @@ let bedsProducts=[
         category: "bed",
     },
     {
-        id: 2,
+        id:5002,
         name: "Essentials Kano Platform Double Bed",
         price: 449.00,
         description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
@@ -17,7 +17,7 @@ let bedsProducts=[
         category: "bed",
     },
     {
-        id: 3,
+        id:5003,
         name: "Hyron Guest Bed with 2 Mattresses",
         price: 339.00,  //after discount //before discount 449
         description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
@@ -25,7 +25,7 @@ let bedsProducts=[
         category: "bed",
     },
     {
-        id: 4,
+        id:5004,
         name: "Kavaro 3000 Pocket Natural Kingsize",
         price: 489.00,  
         description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
@@ -33,7 +33,7 @@ let bedsProducts=[
         category: "bed",
     },
     {
-        id: 5,
+        id:5005,
         name: "Roscoe King Size Bed, Aegean Blue",
         price: 429.00,  
         description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
@@ -42,3 +42,108 @@ let bedsProducts=[
     },
 ]
 // end BEDS products
+// strat sofas products
+let sofasProducts=[
+    {
+        id:6001,
+        name: "Dallas Seater Sofa, Outback Tan",
+        price: 859.00,  //=>after discount //before discount 959.00 //discount is 10%
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/1.jpg",
+        category: "sofa",
+    },
+    {
+        id:6002,
+        name: "Haring Seater Sofa, Azure Blue",
+        price: 1159.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/capture2.jpg",
+        category: "sofa",
+    },
+    {
+        id:6003,
+        name: "Herman Seater Sofa, Finch Grey Cotton",
+        price: 999.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/03.jpg",
+        category: "sofa",
+    },
+    {
+        id:6004,
+        name: "Nya Seater Sofa, Summit Grey Weave",
+        price: 879.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/igp3305_1_-800x600.jpg",
+        category: "sofa",
+    },
+    
+    {
+        id:6005,
+        name: "Reece Seater Sofa, Mina Burnt Orange",
+        price: 599.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/1-1.jpg",
+        category: "sofa",
+    },
+    {
+        id:6006,
+        name: "Ritchie Seater Sofa, Anthracite Grey",
+        price: 1499.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/1-2-600x296.jpg",
+        category: "sofa",
+    },
+    {
+        id:6007,
+        name: "Scott Seater Sofa, Concrete Velvet",
+        price: 699.00,  //after discont //before discount 22% =899.00
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/igp3343-600x450.jpg",
+        category: "sofa",
+    },
+]
+// end sofas products
+// start tabels products
+let tabelsProducts=[
+    {
+        id:7001,
+        name: "Boone 8 Seat Dining Table, Concrete",
+        price: 349.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/14-800x600.jpg",
+        category: "table",
+    },
+    {
+        id:7002,
+        name: "Boone 8 Seat Dining Table, White Concrete",
+        price: 189.00, //=> after discount //before discount 279 .00 -->discount is 32%
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/2-4-800x534.jpg",
+        category: "table",
+    },
+    {
+        id:7003,
+        name: "Copa Garden Lounge Set, Citrus Green",
+        price: 189.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/2-5-600x442.jpg",
+        category: "table",
+    },
+    {
+        id:7004,
+        name: "Elona bedside table, grey and copper",
+        price: 229.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/1-11-800x791.jpg",
+        category: "table",
+    },
+    {
+        id:7005,
+        name: "Jenson 6-8 Seat Oval Extending Dining Table",
+        price: 259.00,  
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/1-8-600x401.jpg",
+        category: "table",
+    },
+]
+// end tabels products
